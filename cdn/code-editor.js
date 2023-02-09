91c666af8f9bb5c58da92c4e5394db85d8a5c8c9
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-02-09 11:07:06 -0800

    Update code-editor.js

--- cdn/code-editor.js
+++ cdn/code-editor.js
@@ -1,1 +1,28 @@
+// Stylesheet
 
+const styles = document.createElement("link");
+styles.rel = "stylesheet"
+styles.href = "ghwosty.github.io/tabcloaker/cdn/code-editor.css"
+
+document.head.appendChild(styles);
+
+//
+
+document.innerHTML = `
+<body>
+<h1>COde Editor</h1>
+
+<textarea width="500px" height="1000px"></textarea>
+</body>
+<br><br>
+<button>Open</button>
+`
+
+setTimeout(() => {
+  document.querySelector("button").addEventListener("click", e);
+}, 500)
+
+function e() {
+  var output = window.open();
+  output.document.write(document.querySelector("textarea").value);
+}
